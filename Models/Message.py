e7ffe109b7b4240165866d27c4fb087def2d2621
Mario <netrix4@gmail.com>
2025-03-23 21:28:37 -0700

    Add new model

---
+++ Models/Message.py
@@ -0,0 +1,10 @@
+class Message:
+    Id: str
+    Title: str
+    Content: str
+    Receiver: int
+    def __init__(self):
+        self.Id = ""
+        self.Title = ""
+        self.Content = ""
+        self.Receiver = ""
