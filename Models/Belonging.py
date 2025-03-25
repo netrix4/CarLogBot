7b690bd005b1ded247a0e8d77ab5fcc046b0af3f
Mario <netrix4@gmail.com>
2025-03-24 22:55:25 -0700

    Add better data estructure for db

--- Models/Belonging.py
+++ Models/Belonging.py
@@ -1,7 +1,8 @@
 class Belonging:
-    Description = str
-    IsLost = bool
-    OwnerId = int
+    Id: int
+    Description: str
+    IsLost: bool
+    OwnerId: int
     def __init__(self):
         self.Description = ""
         self.IsLost = False
--- Models/Car.py
+++ Models/Car.py
@@ -1,11 +1,12 @@
 class Car:
-    Make: str
-    Model: str
-    Color: str
-    OwnerId: int
-    
-    def __init__(self):
-      self.Make = ""
-      self.Model = ""
-      self.Color = ""
-      self.OwnerId = 0
+  Id: int
+  Make: str
+  Model: str
+  Color: str
+  OwnerId: int
+  
+  def __init__(self):
+    self.Make = ""
+    self.Model = ""
+    self.Color = ""
+    self.OwnerId = 0
