fe01b66c1a006fdb2e3a734835e510d84f276f60
Mario <netrix4@gmail.com>
2025-04-05 23:00:32 -0700

    Removed unnesesary code

--- main.py
+++ main.py
@@ -14,24 +14,6 @@ app = ApplicationBuilder().token(api_data.ApiToken).build()
 async def reply_invalid_text(update: Update, context: CallbackContext):
     await update.message.reply_text(f"Invalid command, try the button bellow ⤵️ or these options:\n/whatup\n/dumb\n/kb\n/register\n/new")
 
-async def reply_list_of_qr(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    id_to_list = context._user_id
-    list_of_car_qrs = AddQrController.get_cars(id_to_list)
-    list_of_belongings_qrs = AddQrController.get_belongings(id_to_list)
-
-    formated_results = "Estos son los resultados que encontré:\nCarros 🚗:\n"
-    counter = 0
-    for car in list_of_car_qrs:
-        counter += 1
-        formated_results += f'{counter:02d} - Marca:{car["Make"]} Modelo:{car["Model"]} Color:{car["Color"]}\n'
-    counter = 0
-    formated_results+="\nPertenencias 🎒:\n"
-    for car in list_of_belongings_qrs:
-        counter += 1
-        formated_results += f'{counter:02d} - Descripción:{car["Description"]} ¿Perdido?:{car["IsLost"]}\n'
-
-    await update.message.reply_text(formated_results)
-
 async def reply_whatup(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("So whatup")
     
@@ -48,7 +30,6 @@ async def reply_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app.add_handler(CommandHandler("whatup", reply_whatup))
 app.add_handler(CommandHandler("dumb", reply_as_dumb))
 # app.add_handler(MessageHandler(filters.PHOTO, reply_photo))
-# app.add_handler(CommandHandler("list", reply_list_of_qr))
 
 app.add_handler(ListingController.listing_qrs_controller)
 app.add_handler(ListingController.listing_messages_controller)
